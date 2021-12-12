7e6972e88cb4c9d979d36f3fe8813a3bd4ec5f30
Francesco Malagrino <francesco.malagrino@gmail.com>
2021-12-12 12:20:46 +0100

    Junit test added
    Junit test added

---
+++ app/src/test/java/com/francescomalagrino/dailytrackerapp/util/Constants.java
@@ -0,0 +1,25 @@
+package com.francescomalagrino.dailytrackerapp.util;
+
+import com.francescomalagrino.dailytrackerapp.R;
+
+public class Constants {
+
+        //* moods array images*
+        public static int[] moodImagesArray = {R.drawable.smiley_sad,
+                R.drawable.smiley_disappointed,
+                R.drawable.smiley_normal,
+                R.drawable.smiley_happy,
+                R.drawable.smiley_super_happy};
+        /**
+         * moods array colors
+         */
+
+        public static int[] moodColorsArray = {
+                R.color.colorSad,
+                R.color.colorDisappointed,
+                R.color.colorNormal,
+                R.color.colorHappy,
+                R.color.colorSuperHappy};
+
+
+}
